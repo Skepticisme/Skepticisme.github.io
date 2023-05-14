959302bf2046de8b84ea99035368561f882b3dbc
Skepticisme <73161831+Skepticisme@users.noreply.github.com>
2023-05-14 18:11:27 +0100

    github pages config

--- africoding-front/src/environments/environment.prod.ts
+++ africoding-front/src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
   production: true,
+  auth0: {
+    domain: 'dev-ve5jqdhndo53k8rf.us.auth0.com',
+    clientId: 'wgykHRP8NGn7SqlZb5oe5oGN4MSDucpj',
+    authorizationParams: {
+      redirect_uri: 'http://localhost:4040/callback',
+    },
+    errorPath: '/callback',
+  },
+  api: {
+    serverUrl: 'http://localhost:6060',
+  },
 };
